efecda76df4a14fd9c746763eca67d105427f337
lunadancer@gmail.com <lichking123>
2016-05-13 15:15:13 +0800

    submit

--- src/main/scala/io/github/junheng/akka/monitor/dispatcher/MonitoredForkJoinPool.scala
+++ src/main/scala/io/github/junheng/akka/monitor/dispatcher/MonitoredForkJoinPool.scala
@@ -13,6 +13,10 @@ import scala.concurrent.{ExecutionContext, Future}
 
 class MonitoredForkJoinPool(parallelism: Int, monitorInterval: Long, threadFactory: WorkerThreadFactory, unhandledExceptionHandler: UncaughtExceptionHandler) extends ForkJoinPool(parallelism, threadFactory, unhandledExceptionHandler, true) {
 
+`  MonitoredForkJoinPool.monitoredForkJoinPools += this
+
+  override def shutdown(): Unit = super.shutdown()
+
   override def execute(runnable: Runnable): Unit = {
     if (runnable eq null) {
       throw new NullPointerException
@@ -20,37 +24,42 @@ class MonitoredForkJoinPool(parallelism: Int, monitorInterval: Long, threadFacto
       super.execute(new AkkaForkJoinTask(runnable))
     }
   }
-
-  //monitored thread
-  Future {
-    while (true) {
-      try {
-        if (getFactory != null && MonitoredForkJoinPool.monitor != null) {
-          MonitoredForkJoinPool.monitor(
-            DispatcherStatus(
-              getFactory.asInstanceOf[MonitorableThreadFactory].name,
-              getPoolSize,
-              getActiveThreadCount,
-              getParallelism,
-              getRunningThreadCount,
-              getQueuedSubmissionCount,
-              getQueuedTaskCount,
-              getStealCount
-            )
-          )
-        }
-      } catch {
-        case ex: Exception => ex.printStackTrace()
-      } finally {
-        Thread.sleep(monitorInterval)
-      }
-    }
-  }(ExecutionContext.fromExecutor(Executors.newSingleThreadExecutor()))
 }
 
 object MonitoredForkJoinPool {
   type WorkerThreadFactory = ForkJoinPool.ForkJoinWorkerThreadFactory
   type UncaughtExceptionHandler = Thread.UncaughtExceptionHandler
+
+  protected var monitoredForkJoinPools = Set[MonitoredForkJoinPool]()
+
+  Future {
+    while (true) {
+      try {
+        //update watched list
+        monitoredForkJoinPools = monitoredForkJoinPools.filterNot(p => p.isShutdown || p.isTerminated || p.isTerminating)
+        monitoredForkJoinPools.foreach { p =>
+          if (!p.isShutdown && !p.isTerminated && !p.isTerminating && p.getFactory != null && MonitoredForkJoinPool.monitor != null) {
+            MonitoredForkJoinPool.monitor(
+              DispatcherStatus(
+                p.getFactory.asInstanceOf[MonitorableThreadFactory].name,
+                p.getPoolSize,
+                p.getActiveThreadCount,
+                p.getParallelism,
+                p.getRunningThreadCount,
+                p.getQueuedSubmissionCount,
+                p.getQueuedTaskCount,
+                p.getStealCount
+              )
+            )
+          }
+        }
+      } catch {
+        case ex: Exception => ex.printStackTrace()
+      } finally {
+        Thread.sleep(60000)
+      }
+    }
+  }(ExecutionContext.fromExecutor(Executors.newSingleThreadExecutor()))
 
   var monitor: (DispatcherStatus) => Unit = _ //hook method please replace before dispatcher started
 
